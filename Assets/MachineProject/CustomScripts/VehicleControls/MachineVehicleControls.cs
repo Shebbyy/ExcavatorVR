c1a7a874c55b9b9a48be0de0b38144ff8aef98af
Sebastian Gradwohl <sebiplay15@gmx.at>
2024-11-08 15:32:11 +0100

    Aktueller Zwischenstand; - Hebelsteuerung für Bagger Assets und Scripte angelegt - Erste Versuche den VR Controller beim Greifen des Hebels auf den Hebel zu snappen

---
+++ Assets/MachineProject/CustomScripts/VehicleControls/MachineVehicleControls.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MachineProject.CustomScripts.VehicleControls
+{
+    public abstract class VehicleControls : MonoBehaviour
+    {
+        public abstract void DriveForward();
+
+        public abstract void DriveBackward();
+
+        public abstract void SteerLeft();
+
+        public abstract void SteerRight();
+    }
+
+}
